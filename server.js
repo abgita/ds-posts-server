5e676fb3c28733e22a69ab331a3c7c1be6093b10
abgita <42152656+abgita@users.noreply.github.com>
2020-09-17 00:58:27 -0300

    added some security...

--- server.js
+++ server.js
@@ -1,29 +1,55 @@
-const URL = process.env.URL;
-const PORT = process.env.PORT;
+const { URL, PORT, ORIGIN } = process.env;
 
 const express = require( "express" );
+const express_enforces_ssl = require( 'express-enforces-ssl' );
+const hpp = require( 'hpp' );
 const helmet = require( "helmet" );
+const rateLimit = require( "express-rate-limit" );
 const posts = require( "./posts" );
-
+const { param, body } = require( "express-validator" );
 const { handleError, handleSuccess, validate } = require( "./utils" );
-const { param, body } = require( 'express-validator' );
 
-const validateNewPost = validate( [
-    body( 'stickerId' ).isLength( { min: 15, max: 17 } ).trim().escape(),
+const validateNewPostInput = validate( [
+    body( 'stickerId' ).isLength( { min: 15, max: 18 } ).trim().escape(),
     body( 'trackId' ).isLength( { min: 15, max: 25 } ).trim().escape()
 ] );
 
-const validateGetPost = validate( [
-    param( 'id' ).isLength( { min: 15, max: 35 } ).trim().escape()
+const validateGetPostInput = validate( [
+    param( 'id' ).isLength( { min: 40 } ).trim().escape().custom( value => {
+        const pair = value.split( ':' );
+
+        if ( pair.length !== 2 ) return Promise.reject();
+
+        const xl = pair[0].length;
+        const yl = pair[1].length;
+
+        if ( xl >= 15 && xl <= 18 && yl >= 15 && yl <= 25 ) {
+            return Promise.resolve( value );
+        }
+
+        return Promise.reject();
+    } )
 ] );
 
 function run() {
     const app = express();
 
+    app.enable( 'trust proxy' );
+
     app.use( helmet() );
+
+    app.use( rateLimit( {
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+    } ) );
+
+    app.use( express_enforces_ssl() );
+    app.use( hpp({}) );
     app.use( express.json() );
 
-    app.post( "/", validateNewPost, async ( req, res ) => {
+    app.post( "/", validateNewPostInput, async ( req, res ) => {
+        console.log( req );
+
         const stickerId = req.body.stickerId;
         const trackId = req.body.trackId;
 
@@ -36,14 +62,8 @@ function run() {
         }
     } );
 
-    app.get( "/post/:id", validateGetPost, async ( req, res ) => {
-        try {
-            const post = await posts.getPost( req.params.id );
-
-            handleSuccess( res, post );
-        } catch ( err ) {
-            handleError( res, err );
-        }
+    app.get( "/", async ( req, res ) => {
+        res.sendStatus( 404 );
     } );
 
     app.get( "/latest", async ( req, res ) => {
@@ -61,6 +81,16 @@ function run() {
             const posts_ = await posts.getMostUsed();
 
             handleSuccess( res, posts_ );
+        } catch ( err ) {
+            handleError( res, err );
+        }
+    } );
+
+    app.get( "/:id", validateGetPostInput, async ( req, res ) => {
+        try {
+            const post = await posts.getPost( req.params.id );
+
+            handleSuccess( res, post );
         } catch ( err ) {
             handleError( res, err );
         }
