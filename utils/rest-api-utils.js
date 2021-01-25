de5c98a9235a5d33f3d0ec56620e69db802e9fd6
abgita <42152656+abgita@users.noreply.github.com>
2021-01-25 13:50:34 -0300

    allowMethods handler added

--- utils/rest-api-utils.js
+++ utils/rest-api-utils.js
@@ -1,44 +1,44 @@
 "use strict";
 
-const { json, urlencoded } = require( "express" );
-const express_enforces_ssl = require( "express-enforces-ssl" );
-const helmet = require( "helmet" );
-const rateLimit = require( "express-rate-limit" );
-const hpp = require( "hpp" );
-const { handleError } = require( "./server-utils" );
+const {json, urlencoded} = require("express");
+const express_enforces_ssl = require("express-enforces-ssl");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const {handleError} = require("./server-utils");
 
 const ORIGIN = process.env.ORIGIN;
 
 module.exports = {
-    setupMiddlewares: function ( expressApp, ...middlewares ) {
-        if ( process.env.NODE_ENV !== "development" ) {
-            expressApp.enable( "trust proxy" );
+    setupMiddlewares: function (expressApp, ...middlewares) {
+        if (process.env.NODE_ENV !== "development") {
+            expressApp.enable("trust proxy");
 
-            expressApp.use( express_enforces_ssl() );
+            expressApp.use(express_enforces_ssl());
         }
 
-        expressApp.use( helmet() );
+        expressApp.use(helmet());
 
-        expressApp.use( rateLimit( {
+        expressApp.use(rateLimit({
             windowMs: 15 * 60 * 1000, // 15 minutes
             max: 100, // limit each IP to 100 requests per windowMs
-        } ) );
+        }));
 
-        expressApp.use( hpp( {} ) );
-        expressApp.use( urlencoded( { limit: 100, parameterLimit: 2 } ) );
-        expressApp.use( json( { limit: 100 } ) );
+        expressApp.use(hpp({}));
+        expressApp.use(urlencoded({limit: 100, parameterLimit: 2}));
+        expressApp.use(json({limit: 100}));
 
-        expressApp.use( ( err, req, res, next ) => {
-            if ( err.statusCode === 413 ) {
-                handleError( res, null, null, err.statusCode );
+        expressApp.use((err, req, res, next) => {
+            if (err.statusCode === 413) {
+                handleError(res, null, null, err.statusCode);
             } else {
                 next();
             }
-        } );
+        });
 
-        if ( middlewares && middlewares.length > 0 ) {
-            for ( let middleware of middlewares ) {
-                expressApp.use( middleware );
+        if (middlewares && middlewares.length > 0) {
+            for (let middleware of middlewares) {
+                expressApp.use(middleware);
             }
         }
 
@@ -46,8 +46,16 @@ module.exports = {
     },
 
     allowOrigin: origin => {
-        return function ( req, res, next ) {
-            res.header( "Access-Control-Allow-Origin", origin || ORIGIN );
+        return function (req, res, next) {
+            res.header("Access-Control-Allow-Origin", origin || ORIGIN);
+
+            next();
+        }
+    },
+
+    allowMethods: methods => {
+        return function (req, res, next) {
+            res.header("Access-Control-Allow-Methods", methods.join(" "));
 
             next();
         }
